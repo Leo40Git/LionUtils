56f5e2ea12e8ae6f1b5a05bda49eb43e6c105687
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2020-08-05 13:35:20 +0300

    Add NbtUtil
    GameProfileUtil: improve handling of incomplete profiles (AKA sessionService.fillProfileProperties failing)
    ConfigHolder: remove some @NonNulls

--- src/main/java/adudecalledleo/lionutils/GameProfileUtil.java
+++ src/main/java/adudecalledleo/lionutils/GameProfileUtil.java
@@ -3,6 +3,7 @@ package adudecalledleo.lionutils;
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.minecraft.MinecraftSessionService;
 import net.minecraft.util.UserCache;
+import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
@@ -16,8 +17,11 @@ public final class GameProfileUtil {
         InitializerUtil.badConstructor();
     }
 
+    private static final Logger LOGGER = LoggerUtil.getLogger("LionUtils|GameProfileUtil");
+
     private static MinecraftSessionService sessionService;
     private static UserCache userCache;
+    private static boolean warnedAboutIncompleteProfile = false;
 
     /**
      * Sets the session service that we should use.<br>
@@ -56,30 +60,37 @@ public final class GameProfileUtil {
             profile = userCache.findByName(name);
         if (profile == null) {
             // cache miss, get from session service and add to cache
-            profile = new GameProfile(id, name);
-            sessionService.fillProfileProperties(profile, true);
-            userCache.add(profile);
+            profile = sessionService.fillProfileProperties(new GameProfile(id, name), true);
+            // warn about incomplete profile
+            // the reason for this "warned" boolean is so we don't spam the log if the player has no internet (SP)
+            if (!warnedAboutIncompleteProfile && !profile.isComplete()) {
+                warnedAboutIncompleteProfile = true;
+                LOGGER.warn("getGameProfile: sessionService.fillProfileProperties failed, profile is incomplete. " +
+                        "id = {}, name = \"{}\"", "{" + id + "}", name);
+            } else if (profile.isComplete()) {
+                warnedAboutIncompleteProfile = false;
+                userCache.add(profile);
+            }
         }
         return profile;
     }
 
     /**
      * Default return value of {@link #getPlayerName(UUID)} if something goes wrong.<br>
-     * This name is illegal, as it contains characters which are not allowed in normal usernames.
+     * @implNote This name is illegal, as it contains characters which are not normally allowed in names.
      */
     public static final String PLAYER_NAME_UNKNOWN = "<???>";
 
     /**
      * Gets a player's name from their UUID.
      * @param playerUUID player UUID
-     * @return player name, or {@link #PLAYER_NAME_UNKNOWN} if something went wrong (like the player not existing)
+     * @return the player's name, or {@link #PLAYER_NAME_UNKNOWN} if something went wrong (like the player not existing)
      */
     @NotNull
     public static String getPlayerName(@NotNull UUID playerUUID) {
         GameProfile profile = getGameProfile(playerUUID, null);
-        String name = profile.getName();
-        if (name == null)
-            name = PLAYER_NAME_UNKNOWN;
-        return name;
+        if (!profile.isComplete())
+            return PLAYER_NAME_UNKNOWN;
+        return profile.getName();
     }
 }
---
+++ src/main/java/adudecalledleo/lionutils/serialize/NbtUtil.java
@@ -0,0 +1,97 @@
+package adudecalledleo.lionutils.serialize;
+
+import adudecalledleo.lionutils.InitializerUtil;
+import com.google.gson.JsonElement;
+import com.mojang.serialization.JsonOps;
+import net.minecraft.datafixer.NbtOps;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.util.math.BlockPos;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Helper class for dealing with reading from and writing to NBT tags.
+ */
+public final class NbtUtil {
+    private NbtUtil() {
+        InitializerUtil.badConstructor();
+    }
+
+    /**
+     * Converts an NBT tag into a {@link JsonElement}.
+     * @param tag source tag
+     * @return the JSON element
+     */
+    public static JsonElement toJson(Tag tag) {
+        return NbtOps.INSTANCE.convertTo(JsonOps.INSTANCE, tag);
+    }
+
+    /**
+     * Converts a {@link JsonElement} into an NBT tag.
+     * @param element source elemnt
+     * @return the NBT tag
+     */
+    public static Tag fromJson(JsonElement element) {
+        return JsonOps.INSTANCE.convertTo(NbtOps.INSTANCE, element);
+    }
+
+    /**
+     * Reads a {@link BlockPos} from a {@link CompoundTag}.
+     * @param tag source tag
+     * @param key key to read from
+     * @return the block position, or {@code null} if the key isn't associated with one
+     */
+    public static @Nullable BlockPos getBlockPos(CompoundTag tag, String key) {
+        if (!tag.contains(key, /* NbtType.LONG */ 4))
+            return null;
+        return BlockPos.fromLong(tag.getLong(key));
+    }
+
+    /**
+     * Writes a {@link BlockPos} to a {@link CompoundTag}.
+     * @param tag destination tag
+     * @param key key to write to
+     * @param pos block position to write
+     */
+    public static void setBlockPos(CompoundTag tag, String key, BlockPos pos) {
+        tag.putLong(key, pos.asLong());
+    }
+
+    private static final BlockPos[] EMPTY_POS_ARRAY = new BlockPos[0];
+
+    /**
+     * Reads a {@link BlockPos} array from a {@link CompoundTag}.
+     * @param tag source tag
+     * @param key key to read from
+     * @return the block position array, or an empty array if the key isn't associated with one
+     */
+    public static BlockPos[] getBlockPosArray(CompoundTag tag, String key) {
+        if (!tag.contains(key, /* NbtType.LONG_ARRAY */ 12))
+            return EMPTY_POS_ARRAY;
+        return Arrays.stream(tag.getLongArray(key)).mapToObj(BlockPos::fromLong).toArray(BlockPos[]::new);
+    }
+
+    /**
+     * Writes a {@link BlockPos} array to a {@link CompoundTag}.
+     * @param tag destination tag
+     * @param key key to write to
+     * @param posArr block position array to write
+     */
+    public static void setBlockPosArray(CompoundTag tag, String key, BlockPos... posArr) {
+        tag.putLongArray(key, Arrays.stream(posArr).mapToLong(BlockPos::asLong).toArray());
+    }
+
+    /**
+     * Writes a {@link BlockPos} list to a {@link CompoundTag}. The list is treated as an array.
+     * @param tag destination tag
+     * @param key key to write to
+     * @param posList block position list to write
+     * @see #setBlockPosArray(CompoundTag, String, BlockPos...)
+     */
+    public static void setBlockPosArray(CompoundTag tag, String key, List<BlockPos> posList) {
+        setBlockPosArray(tag, key, posList.toArray(new BlockPos[0]));
+    }
+}
