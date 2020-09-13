32aa2e06bdd036c53dc5d1f93e334c3a7c432246
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2020-09-13 11:41:03 +0300

    serialize.NbtUtil: Add methods for handling enum values
    Declare compatibility with 1.16.3

--- src/main/java/adudecalledleo/lionutils/serialize/NbtUtil.java
+++ src/main/java/adudecalledleo/lionutils/serialize/NbtUtil.java
@@ -57,7 +57,7 @@ public final class NbtUtil {
      * @param pos
      *         block position to write
      */
-    public static void setBlockPos(CompoundTag tag, String key, BlockPos pos) {
+    public static void putBlockPos(CompoundTag tag, String key, BlockPos pos) {
         tag.putLong(key, pos.asLong());
     }
 
@@ -101,7 +101,7 @@ public final class NbtUtil {
      * @param posArr
      *         block position array to write
      */
-    public static void setBlockPosArray(CompoundTag tag, String key, BlockPos... posArr) {
+    public static void putBlockPosArray(CompoundTag tag, String key, BlockPos... posArr) {
         tag.putLongArray(key, Arrays.stream(posArr).mapToLong(BlockPos::asLong).toArray());
     }
 
@@ -114,9 +114,125 @@ public final class NbtUtil {
      *         key to write to
      * @param posList
      *         block position list to write
-     * @see #setBlockPosArray(CompoundTag, String, BlockPos...)
+     * @see #putBlockPosArray(CompoundTag, String, BlockPos...)
      */
+    public static void putBlockPosArray(CompoundTag tag, String key, List<BlockPos> posList) {
+        putBlockPosArray(tag, key, posList.toArray(new BlockPos[0]));
+    }
+
+    private static <T extends Enum<T>> T getEnum0(Class<T> enumType, CompoundTag tag, String key) {
+        try {
+            return T.valueOf(enumType, tag.getString(key));
+        } catch (IllegalArgumentException ignored) {
+            return null;
+        }
+    }
+
+    /**
+     * Checks if a {@link CompoundTag} contains an {@code enum} value.
+     *
+     * @param enumType
+     *         type of {@code enum} value
+     * @param tag
+     *         source tag
+     * @param key
+     *         key to check
+     * @param <T>
+     *         type of {@code enum} value
+     * @return {@code true} if {@code enum} value is present, {@code false} otherwise
+     *
+     * @since 5.1.0
+     */
+    public static <T extends Enum<T>> boolean containsEnum(Class<T> enumType, CompoundTag tag, String key) {
+        if (!tag.contains(key, /* NbtType.STRING */ 8))
+            return false;
+        return getEnum0(enumType, tag, key) != null;
+    }
+
+    /**
+     * Reads an {@code enum} value from a {@link CompoundTag}.
+     *
+     * @param enumType
+     *         type of {@code enum} value
+     * @param tag
+     *         source tag
+     * @param key
+     *         key to read from
+     * @param <T>
+     *         type of {@code enum} value
+     * @return the {@code enum} value, or {@code null} if the key isn't associated with one
+     *
+     * @since 5.1.0
+     */
+    public static <T extends Enum<T>> T getEnum(Class<T> enumType, CompoundTag tag, String key) {
+        if (!tag.contains(key, /* NbtType.STRING */ 8))
+            return null;
+        return getEnum0(enumType, tag, key);
+    }
+
+    /**
+     * Writes an {@code enum} value to a {@link CompoundTag}.
+     *
+     * @param tag
+     *         destination tag
+     * @param key
+     *         key to write to
+     * @param value
+     *         {@code enum} value to write
+     * @param <T>
+     *         type of {@code enum} value
+     * @since 5.1.0
+     */
+    public static <T extends Enum<T>> void putEnum(CompoundTag tag, String key, T value) {
+        tag.putString(key, value.name());
+    }
+
+    /**
+     * Writes a {@link BlockPos} to a {@link CompoundTag}.
+     *
+     * @param tag
+     *         destination tag
+     * @param key
+     *         key to write to
+     * @param pos
+     *         block position to write
+     * @deprecated Use {@link #putBlockPos(CompoundTag, String, BlockPos)} instead.
+     */
+    @Deprecated
+    public static void setBlockPos(CompoundTag tag, String key, BlockPos pos) {
+        putBlockPos(tag, key, pos);
+    }
+
+    /**
+     * Writes a {@link BlockPos} array to a {@link CompoundTag}.
+     *
+     * @param tag
+     *         destination tag
+     * @param key
+     *         key to write to
+     * @param posArr
+     *         block position array to write
+     * @deprecated Use {@link #putBlockPosArray(CompoundTag, String, BlockPos...)} instead.
+     */
+    @Deprecated
+    public static void setBlockPosArray(CompoundTag tag, String key, BlockPos... posArr) {
+        putBlockPosArray(tag, key, posArr);
+    }
+
+    /**
+     * Writes a {@link BlockPos} list to a {@link CompoundTag}. The list is treated as an array.
+     *
+     * @param tag
+     *         destination tag
+     * @param key
+     *         key to write to
+     * @param posList
+     *         block position list to write
+     * @see #setBlockPosArray(CompoundTag, String, BlockPos...)
+     * @deprecated Use {@link #putBlockPosArray(CompoundTag, String, List)} instead.
+     */
+    @Deprecated
     public static void setBlockPosArray(CompoundTag tag, String key, List<BlockPos> posList) {
-        setBlockPosArray(tag, key, posList.toArray(new BlockPos[0]));
+        putBlockPosArray(tag, key, posList);
     }
 }
